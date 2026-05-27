c7efe286a4f1abb4223513df79ed5ceeab3e0f8d
Naix <91845220+NaixGames@users.noreply.github.com>
2026-05-27 19:59:34 +0100

    Doing split for images and url. Adfing tests

--- src/split_utils.py
+++ src/split_utils.py
@@ -49,3 +49,64 @@ def extract_markdown_links(text: str) -> list[tuple[str,str]]:
         result.append(img)
 
     return result
+
+def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
+    answer = []
+    for node in old_nodes:
+        processed_text = node.text
+        extracted_images = extract_markdown_images(processed_text)
+
+        if len(extracted_images) == 0:
+            answer.append(node)
+            continue
+        
+        for image_ext in extracted_images:
+            splited_text = processed_text.split(f"![{image_ext[0]}]({image_ext[1]})", 1)
+
+            first_half = splited_text[0]
+            if (first_half != ""):
+                created_node = TextNode(first_half, TextType.TEXT)
+                answer.append(created_node)
+
+            created_node = TextNode(image_ext[0], TextType.IMAGE,  image_ext[1])
+            answer.append(created_node)
+
+            processed_text = splited_text[1]
+                
+                
+        if (processed_text != ""):
+            created_node = TextNode(processed_text, TextType.TEXT)
+            answer.append(created_node)
+
+    return answer
+    
+
+def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
+    answer = []
+    for node in old_nodes:
+        processed_text = node.text
+        extracted_links = extract_markdown_links(processed_text)
+
+        if len(extracted_links) == 0:
+            answer.append(node)
+            continue
+        
+        for url_ext in extracted_links:
+            splited_text = processed_text.split(f"[{url_ext[0]}]({url_ext[1]})", 1)
+
+            first_half = splited_text[0]
+            if (first_half != ""):
+                created_node = TextNode(first_half, TextType.TEXT)
+                answer.append(created_node)
+
+            created_node = TextNode(url_ext[0], TextType.LINK,  url_ext[1])
+            answer.append(created_node)
+
+            processed_text = splited_text[1]
+                
+                
+        if (processed_text != ""):
+            created_node = TextNode(processed_text, TextType.TEXT)
+            answer.append(created_node)
+
+    return answer
