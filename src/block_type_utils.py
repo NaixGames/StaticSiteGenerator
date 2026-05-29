7ffc4d6161312b8f5ffe7f6ff42d87a5d099a83d
Naix <91845220+NaixGames@users.noreply.github.com>
2026-05-29 19:45:54 +0100

    Added block to block type and tests for it

---
+++ src/block_type_utils.py
@@ -0,0 +1,74 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+def is_heading(text: str) -> bool:
+    if (text[0] != "#"):
+        return False
+    
+    for i in range(1, 7):
+        if i >= len(text):
+            return False
+        
+        if text[i] == " ":
+            return i < len(text) - 1
+        
+        if text[i] != "#":
+            return False
+
+        
+def is_code_block(text: str) -> bool:
+    if (text[0:4] != "```\n"):
+        return False
+    
+    if (text[-3:] != "```" and text[-4:] != "```\n"):
+        return False
+    
+    return True
+
+def is_quote_block(text: str) -> bool:
+    return text[0] == ">"
+
+def is_unordered_list(text: str) -> bool:
+    splited_lines = text.split("\n")
+
+    for line in splited_lines:
+        if line[0: 2] != "- ":
+            return False
+        
+    return True
+
+def is_ordered_list(text: str) -> bool:
+    splited_lines = text.split("\n")
+
+    for i in range(0, len(splited_lines)):
+        line = splited_lines[i]
+
+        start_ind = line.find(", ")
+        if (start_ind == -1):
+            return False
+        
+        if line[0:start_ind + 2] != f"{i+1}, ":
+            return False
+        
+    return True
+
+def block_to_block_type(text : str) -> BlockType:
+    if (is_heading(text)):
+        return BlockType.HEADING
+    if (is_code_block(text)):
+        return BlockType.CODE
+    if (is_quote_block(text)):
+        return BlockType.QUOTE
+    if (is_unordered_list(text)):
+        return BlockType.UNORDERED_LIST
+    if (is_ordered_list(text)):
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
---
+++ src/tests/test_block_type_utils.py
@@ -0,0 +1,52 @@
+import unittest
+from inspect import getsourcefile
+import os.path
+import sys
+current_path = os.path.abspath(getsourcefile(lambda:0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+sys.path.insert(0, parent_dir)
+
+from block_type_utils import BlockType, block_to_block_type
+
+
+class TestBlockTypeUtils(unittest.TestCase):
+    def test_block_type_utils(self):
+        text1 = "# This is a heading"
+        text2 = "### This is also a heading"
+        text3 = "#This is not a heading"
+
+        #self.assertEqual(block_to_block_type(text1), BlockType.HEADING)
+        #self.assertEqual(block_to_block_type(text2), BlockType.HEADING)
+        #self.assertEqual(block_to_block_type(text3), BlockType.PARAGRAPH)
+
+        text4 = "```\n This is valid code block text```"
+        text5 = "```\n This is also valid code block text ```\n"
+        text6 = "``` This is not valid code block text ```\n"
+        
+        self.assertEqual(block_to_block_type(text4), BlockType.CODE)
+        self.assertEqual(block_to_block_type(text5), BlockType.CODE)
+        self.assertEqual(block_to_block_type(text6), BlockType.PARAGRAPH)
+
+        text7 = ">This is a quote text"
+        text8 = "> This is also a quote text"
+        text9 = " > Is not a quote text!"
+
+        self.assertEqual(block_to_block_type(text7), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(text8), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(text9), BlockType.PARAGRAPH)
+
+        text10 = "- This is\n- An unordered  list"
+        text11 = "- This is not\n-an unordered list"
+        self.assertEqual(block_to_block_type(text10), BlockType.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type(text11), BlockType.PARAGRAPH)
+
+        text12 = "1, This is\n2, an ordered\n3, list"
+        text13 = "0, This is\n1, not an ordered list"
+        text14 = "0,This is not an ordered list"
+        self.assertEqual(block_to_block_type(text12), BlockType.ORDERED_LIST)
+        self.assertEqual(block_to_block_type(text13), BlockType.PARAGRAPH)
+        self.assertEqual(block_to_block_type(text14), BlockType.PARAGRAPH)
+
+if __name__ == "__main__":
+    unittest.main()
